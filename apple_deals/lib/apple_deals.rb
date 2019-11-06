b65764f2ede2db4413dbea911e90542ed8326a0f
Brianna Jordan <bryvjordan9@gmail.com>
2019-11-06 23:33:17 +0000

    cli edits and error fix

--- apple_deals/lib/apple_deals.rb
+++ apple_deals/lib/apple_deals.rb
@@ -1,3 +1,4 @@
 require_relative  "./apple_deals/version"
 
 require_relative  './cli'
+
--- apple_deals/lib/cli.rb
+++ apple_deals/lib/cli.rb
@@ -1,7 +1,6 @@
 class AppleDeals::CLI
   
   def call 
-    puts "Current Apple Deals:"
     offer
     deal_list
     deal_selection 
@@ -9,33 +8,35 @@ class AppleDeals::CLI
   end 
   
   def offer 
-    puts "Would you like to know about current apple deals for this week?"
-    when "Yes"
+    puts "Hello!, would you like to know about current apple deals?"
+    if "Yes"
       puts deal_list
-    when "No"
+    elsif "No"
       puts " If you ever change your mind, text Apple Deals. "
-    when "Apple Deals"
+    else "Apple Deals"
       puts deal_list
+    end 
   end 
+end
   
   def deal_list 
-    puts <<-DOC..gsub/^\s*/,''
+    puts <<-DOC
     1. New Apple iPad (10.2-Inch, Wi-Fi, 128GB) - Silver (Latest Model)
     2. Apple Watch Series 3 (GPS, 38mm) - Space Gray Aluminium Case with Black Sport Band
     3. Apple AirPods with Charging Case (Latest Model)
     4. Beats by Dr. Dre - Beats Solo³ Wireless Headphones - Rose Gold
     5. Beats by Dr. Dre - urBeats³ Earphones with Lightning Connector - Black
     6. Beats by Dr. Dre - Powerbeats³ Wireless - Black
-  DOC
+    DOC
   end 
   
   def deal_selection
     puts "Enter the number of the deal for more info"
     while input != "exit"
-    input = gets.strip
+    input = gets.strip.downcase
     case input 
     when "1"
-      puts "Deal #1..."
+      puts "Deal #1"
     when "2"
       puts "Deal #2"
     when "3"
@@ -46,10 +47,9 @@ class AppleDeals::CLI
       puts "Deal #5"
     when "No"
       puts "If you ever change your mind, text Apple Deals."
-      binding.pry
-    end 
-  end 
-end 
+    end
+  end
+ 
   def goodbye
     puts "We'll be back with weekly Apple deals!"
   end 
